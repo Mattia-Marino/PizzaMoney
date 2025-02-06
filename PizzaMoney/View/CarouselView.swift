9108ce2b225b99fd4f047726f3fb0f2fe7442173
Armando D'Arienzo <65279261+armyj94@users.noreply.github.com>
2025-02-06 11:42:39 +0100

    CarouselView first implementation

---
+++ PizzaMoney/View/CarouselView.swift
@@ -0,0 +1,54 @@
+//
+//  WalletsCaroueslView.swift
+//  PizzaMoney
+//
+//  Created by Armando on 06/02/25.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    
+    var banks : [String]
+    @Binding var selected : String
+    
+    //TODO: da cambiare quando lo schema dati sarà completato
+    
+    let selectedColor = Color.blue
+    let unselectedColor = Color.gray
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(banks, id: \.self) { bank in
+                    
+                    let color = if selected == bank {
+                        selectedColor
+                    } else {
+                        unselectedColor
+                    }
+                                        
+                    Button(action: {
+                        selected = bank
+                    }) {
+                        Text(bank)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 50, style: .continuous).fill(color)
+                            )
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                    }
+                    
+                }
+            }
+            
+        }.scrollIndicators(.hidden)
+    }
+}
+
+#Preview {
+    @Previewable var banks = ["Contesa San Paolo", "Banca Inulia", "BananaRepublic"]
+    @Previewable @State var selected = "Contesa San Paolo"
+    CarouselView(banks: banks, selected: $selected)
+}
