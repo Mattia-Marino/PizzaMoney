9fcfff26af5132dc94bc95dbda41d63b838babed
kaporaso <ic170499@gmail.com>
2025-02-07 12:40:17 +0100

    picker

--- PizzaMoney/View/SettingsView.swift
+++ PizzaMoney/View/SettingsView.swift
@@ -10,19 +10,29 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         Text("Settings")
-            .font(.system(size: 30, weight: .bold));        Spacer()
+            .font(.system(size: 30, weight: .bold))
+            .fontWeight(.semibold);        Spacer()
         VStack{
             HStack{
                 Text("Appearance")
                     .font(.system(size: 25, weight:       .bold))
                     .fontWeight(.regular)
-                    .padding(/*@START_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.leading, 10.0)
                 .foregroundStyle(Color.gray);               Spacer()}
             Text("Theme")
                 .font(.system(size: 20, weight: .regular))
-                .padding(.top, -19.0)
-            
-            
+                .padding(.top, -5.0)
+                .padding(.trailing, 275.0)
+            Divider()
+                .frame(width: 350.0)
+            Picker(selection: .constant(1), label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                Text("System").tag(1)
+                /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                
+            }
+            .padding(.top, -48.0)
+            .padding(.leading, 250.0)
+
             Spacer()
         }
     }
