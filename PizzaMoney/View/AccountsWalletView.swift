44f7bd08a3f1f54e250bafbef0b2d9cc8ea06099
Mattia Marino <mattia.marino01@gmail.com>
2025-02-07 10:42:30 +0100

    Created file for account view

---
+++ PizzaMoney/View/AccountsWalletView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountsWalletView.swift
+//  PizzaMoney
+//
+//  Created by san015 on 07/02/25.
+//
+
+import SwiftUI
+
+struct AccountsWalletView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountsWalletView()
+}
