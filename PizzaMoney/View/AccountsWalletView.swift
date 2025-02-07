6aa6e9e485bccc0dd67c7d544ecb07831157c8ab
Mattia Marino <mattia.marino01@gmail.com>
2025-02-07 12:18:43 +0100

    Card for accounts with button to hide

--- PizzaMoney/View/AccountsWalletView.swift
+++ PizzaMoney/View/AccountsWalletView.swift
@@ -9,22 +9,41 @@ import SwiftUI
 
 struct AccountsWalletView: View {
     var value: Double
+    @State private var isValueVisible: Bool = true
 
     var body: some View {
         ZStack {
             Color(cardColor(for: value))
-                .cornerRadius(10)
+                .cornerRadius(20)
                 .shadow(radius: 5)
             
-            VStack {
-                Text("Card Value: \(String(format: "%.2f", value))")
-                    .font(.headline)
-                    .foregroundColor(.white)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Total")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Button(action: {
+                        isValueVisible.toggle()
+                    }) {
+                        Image(systemName: isValueVisible ? "eye" : "eye.slash")
+                            .foregroundColor(.black)
+                    }
+                }
+                Spacer()
+                if isValueVisible {
+                    HStack {
+                        Spacer()
+                        Text("Card value: \(String(format: "%.2f", value))")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        Spacer()
+                    }
+                }
             }
             .padding()
         }
-        .frame(width: 200, height: 100)
-        Spacer()
+        .frame(width: 360, height: 170)
     }
 
     func cardColor(for value: Double) -> UIColor {
@@ -32,11 +51,11 @@ struct AccountsWalletView: View {
         case ...0:
             return .red
         default:
-            return .green
+            return .greenAccounts
         }
     }
 }
 
 #Preview {
-    AccountsWalletView(value: 0)
+    AccountsWalletView(value: 400000)
 }
