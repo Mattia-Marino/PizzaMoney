15f4ed2dc06f403115fc72fd14d51a1d0213a445
darknifemaster <rockstargold18@gmail.com>
2025-02-07 10:04:47 +0100

    EditCategoriesView
    create a new file

---
+++ PizzaMoney/View/EditCategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  EditCategoriesView.swift
+//  PizzaMoney
+//
+//  Created by san023 on 07/02/25.
+//
+
+import SwiftUI
+
+struct EditCategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCategoriesView()
+}
