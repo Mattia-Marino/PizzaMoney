7a64011cbdec92cd2ab19129d39baa9db6b35523
kaporaso <ic170499@gmail.com>
2025-02-07 12:12:46 +0100

    settings, appearance, theme
    theme da finire

--- PizzaMoney/View/SettingsView.swift
+++ PizzaMoney/View/SettingsView.swift
@@ -10,8 +10,25 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         Text("Settings")
+            .font(.system(size: 30, weight: .bold));        Spacer()
+        VStack{
+            HStack{
+                Text("Appearance")
+                    .font(.system(size: 25, weight:       .bold))
+                    .fontWeight(.regular)
+                    .padding(/*@START_MENU_TOKEN@*/)
+                .foregroundStyle(Color.gray);               Spacer()}
+            Text("Theme")
+                .font(.system(size: 20, weight: .regular))
+                .padding(.top, -19.0)
+            
+            
+            Spacer()
+        }
     }
 }
+            
+        
 
 #Preview {
     SettingsView()
