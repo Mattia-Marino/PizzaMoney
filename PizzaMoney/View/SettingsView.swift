7b048b41e8b6370940675317dd9b3da9f40f319c
kaporaso <ic170499@gmail.com>
2025-02-11 10:00:22 +0100

    Update SettingsView.swift

--- PizzaMoney/View/SettingsView.swift
+++ PizzaMoney/View/SettingsView.swift
@@ -9,49 +9,72 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Settings")
-            .font(.system(size: 30, weight: .bold))
-            .fontWeight(.semibold);        Spacer()
-        VStack{
-            HStack{
-                Text("Appearance")
-                    .font(.system(size: 25, weight:       .bold))
-                    .fontWeight(.regular)
-                    .padding(/*@START_MENU_TOKEN@*/.leading, 10.0)
-                .foregroundStyle(Color.gray);               Spacer()}
-            Text("Theme")
-                .font(.system(size: 20, weight: .regular))
-                .padding(.top, -5.0)
-                .padding(.trailing, 275.0)
-            Divider()
-                .frame(width: 350.0)
-            Picker(selection: .constant(1), label: Text("Picker"))
-            {
-                Text("System").tag(1)
-                /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
-                
-            }
+        NavigationStack{
             
-            .padding(.top, -48.0)
-            .padding(.leading, 250.0)
-            Spacer()}
-        
             VStack{
                 HStack{
-                    Text("Tools")
+                    Text("Appearance")
                         .font(.system(size: 25, weight:       .bold))
                         .fontWeight(.regular)
-                        .padding(.bottom, 600.0)
-                        .padding(.leading, 14.0)
-                        .foregroundStyle(Color.gray);
+                        .padding(/*@START_MENU_TOKEN@*/.leading, 10.0)
+                    .foregroundStyle(Color.gray);               Spacer()}
+                
+                HStack{
+                    Text("Theme")
+                        .font(.system(size: 20, weight: .regular))
+                        .padding(.leading, 10.0)
+                    
                     Spacer()
+                    
+                    Picker(selection: .constant(1), label: Text("Picker"))
+                    {
+                        Text("System").tag(1)
+                        /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                        
+                    }.padding(.trailing, 10.0)
+                   
+                    }
+                    
+                    Divider()
+                    
+                    
+                    
+                    
+                    HStack{
+                        Text("Tools")
+                            .font(.system(size: 25, weight:       .bold))
+                            .fontWeight(.regular)
+                            .padding(.leading, 14.0)
+                            .foregroundStyle(Color.gray);
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    Divider()
+                    
+                    
+                    Spacer()
+                    
                 }
+                
+                
+                
+                
+                
+                .navigationTitle(Text("Settings"))
+                .navigationBarTitleDisplayMode(.inline)
             }
+            
         }
+        
     }
+
     
     
     
-    #Preview {
-        SettingsView()
-    }
+    
+#Preview {
+    SettingsView()
+}
+
